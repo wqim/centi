4fd578914f9f0c5e93698bf97aa52edcfc83c7dc
wqim <joseph3834@proton.me>
2025-11-10 16:09:32 +0000

    Update config.go

--- config/config.go
+++ config/config.go
@@ -3,81 +3,74 @@ package config
 import (
 	"os"
 	//"strings"
-	"encoding/json"
-
+	//"encoding/json"
+	"gopkg.in/yaml.v3"
+	
 	"centi/cryptography"
 	"centi/util"
 )
 
 // general structure of channel
 type Channel struct {
-	Name	string				`json:"name"`
-	Args	map[string]string		`json:"args"`
+	Name	string				`json:"name"`	// name of channel to use for communications
+	Args	map[string]string		`json:"args"`	// optional arguments for this channel
 }
 
 // general information about connection
 type ConnectionInfo struct {
-	Platform	string			`json:"platform"`
-	Args		map[string]string	`json:"args"`
-	Channels	[]Channel		`json:"channels"`
-}
-
-// public key distribution parameters
-type DistributionParameters struct {
-	Type	uint8			`json:"type"` // type of public key distribution
-	Args	map[string]string	`json:"args"` // optional arguments for platform(s)
+	Platform	string			`json:"platform"`	// platform name
+	Args		map[string]string	`json:"args"`		// optional arguments for Connection constructor
+	Channels	[]Channel		`json:"channels"`	// a list of channels to use over this connection
 }
 
 // basic network configuration
 type NetworkConfig struct {
 
 	// delays between messages in the queue
-	MinDelay	uint	`json:"min_delay"`
-	MaxDelay	uint	`json:"max_delay"`
-	CollectionDelay	uint	`json:"keys_collection_delay"` // delay between public keys collection
+	MinDelay	uint	`yaml:"min_delay"`
+	MaxDelay	uint	`yaml:"max_delay"`
+	CollectionDelay	uint	`yaml:"keys_collection_delay"` // delay between public keys collection
 
 	// the size of messages queue
-	QueueSize	uint	`json:"queue_size"`
+	QueueSize	uint	`yaml:"queue_size"`
 
 	// the size of packet
-	PacketSize	uint	`json:"packet_size"`
+	PacketSize	uint	`yaml:"packet_size"`
 
 	// some security and privacy-related parameters
-	AcceptUnknown	bool	`json:"accept_unknown"`              // to be or not to be a 'receiver' during key exchange
-	SendKnownPeers	bool	`json:"send_known_peers"`            // if true, allows resending of known public keys
+	AcceptUnknown	bool	`yaml:"accept_unknown"`              // to be or not to be a 'receiver' during key exchange
+	SendKnownPeers	bool	`yaml:"send_known_peers"`            // if true, allows resending of known public keys
 
-	// mode in which we distribute ephemerial key dufing key exchange instead of real one
+	// mode in which we distribute ephemerial key during key exchange instead of real one
 	// pretty useful to hide us from destination
-	EphemeralMode	bool	`json:"ephemeral_mode"`
+	//EphemeralMode	bool	`yaml:"ephemeral_mode"`	// bullshit, now will always be true
 
 	// network key, required to prevent mitm attacks.
-	// this one is used while distributing public key and must not be chaned even in ephemeral mode.
-	NetworkKey	string			`json:"network_key"`
-	// subkey for every peer, to protect from identify theft attacks.
-	NetworkSubkeys	map[string]string	`json:"network_subkeys"`
-	DistrParams	DistributionParameters `json:"key_distribution_parameters"` // parameters of public key ditribution
+	// this one is used while distributing public key and must not be changed even in ephemeral mode.
+	NetworkKey	string		`yaml:"network_key"`	// more security for closed communities.
+	Peers		[]string	`yaml:"peers"`		// hashes of peer's public keys.
 }
 
 /*
  * Server configuration - configuration of local API server.
- * Ususally contains a bunch of pages to serve. These pages can be
+ * Usually contains a bunch of pages to serve. These pages can be
  * network applications like chats, emails, etc.
  * There are some pages which set up by default, but for developers'
- * convinience configuration is available.
+ * convenience configuration is available.
  */
 type ServerConfiguration struct {
-	Address		string			`json:"address"`
-	NotFoundPage	string			`json:"not_found_page"`
-	Pages		map[string]string	`json:"pages"`
+	Address		string			`yaml:"address"`
+	NotFoundPage	string			`yaml:"not_found_page"`
+	Pages		map[string]string	`yaml:"pages"`
 }
 
 /*
  * Configuration of keys and other client-related information stored.
  */
 type KeysConfig struct {
-	Pk		string			`json:"public_key"`
-	Sk		string			`json:"private_key"`
-	Peers		map[string][]string	`json:"peers"`
+	Pk		string			`yaml:"public_key"`
+	Sk		string			`yaml:"private_key"`
+	//Peers		map[string][]string	`yaml:"peers"`
 }
 
 /*
@@ -85,7 +78,7 @@ type KeysConfig struct {
  * but can contain steganography methods restrictions in the future.
  */
 type SteganoConfig struct {
-	Folder		string			`json:"decoy_files_folder"`
+	Folder		string			`yaml:"decoy_files_folder"`
 }
 
 /*
@@ -93,17 +86,20 @@ type SteganoConfig struct {
  * a lot of things like building subnetworks based on specified parameters.
  */
 type FullConfig struct {
-	NetworkConfig	NetworkConfig		`json:"network_config"`
-	ServerConfig	ServerConfiguration	`json:"local_server_config"`
-	StegConfig	SteganoConfig		`json:"steganography_config"`
-	Logger		util.LoggerInfo		`json:"logger_config"`
-	PlatformsData	[]ConnectionInfo	`json:"platforms_data"`
-	DbFile		string			`json:"db_file"`
-	DbPassword	string			`json:"db_password"`
-	DbRowsLimit	uint			`json:"db_rows_limit"`
-	Keys		KeysConfig		`json:"keys"`
+	NetworkConfig	NetworkConfig		`yaml:"network_config"`
+	ServerConfig	ServerConfiguration	`yaml:"local_server_config"`
+	StegConfig	SteganoConfig		`yaml:"steganography_config"`
+	Logger		util.LoggerInfo		`yaml:"logger_config"`
+	PlatformsData	[]ConnectionInfo	`yaml:"platforms_data"`
+	DbFile		string			`yaml:"db_file"`
+	DbPassword	string			`yaml:"db_password"`
+	DbRowsLimit	uint			`yaml:"db_rows_limit"`
+	Keys		KeysConfig		`yaml:"keys"`
 }
 
+/*
+ * Functions for loading and saving configuration in YAML format.
+ */
 func LoadConfig(filename string, key []byte) (*FullConfig, error) {
 	data, err := LoadEncrypted(filename, key)
 	if err != nil {
@@ -111,23 +107,23 @@ func LoadConfig(filename string, key []byte) (*FullConfig, error) {
 	}
 
 	var conf FullConfig
-	if err := json.Unmarshal(data, &conf); err != nil {
+	if err := yaml.Unmarshal(data, &conf); err != nil {
 		return nil, err
 	}
 	return &conf, nil
 }
 
-/*
- * TODO: change format to xjson or yaml for more readability.
- */
 func SaveConfig(filename string, key []byte, c *FullConfig) error {
-	data, err := json.MarshalIndent(*c, "", "\t")
+	data, err := yaml.Marshal( *c ) //json.MarshalIndent(*c, "", "\t")
 	if err != nil {
 		return err
 	}
 	return SaveEncrypted(filename, key, data)
 }
 
+/*
+ * Functions for saving and loading encrypted files.
+ */
 func LoadEncrypted(filename string, key []byte) ([]byte, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -161,7 +157,11 @@ func ExtractNetworkSubkeys( networkSubkeys map[string]string ) map[string][]byte
 	for k, v := range networkSubkeys {
 		pass, saltBytes, err := cryptography.SplitWithSalt( v )
 		if err == nil {
-			result[ k ] = cryptography.DeriveKey( pass, saltBytes )
+			//result[ k ] = cryptography.DeriveKey( pass, saltBytes )
+			tmp, err := cryptography.DeriveKey( pass, saltBytes )
+			if err == nil {
+				result[ k ] = tmp
+			}
 		}
 	}
 	return result
