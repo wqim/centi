e6f9179fb6bcdc89d298d35acd36da124c0ac2ff
wqim <joseph3834@proton.me>
2025-11-10 16:22:04 +0000

    Update callbacks.py

--- microservices/rns/callbacks.py
+++ microservices/rns/callbacks.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import RNS
-#from colorama import Fore
+from colorama import Fore
 from globals import *
 
 def client_connected( link ):
@@ -9,7 +9,7 @@ def client_connected( link ):
     while have_public_key() == False:
         pass
 
-    send_public_key( link )
+    #send_public_key( link )
     set_callbacks( link, packet_received )
     add_peer( link )
 
@@ -21,8 +21,8 @@ def packet_received( message, packet ):
     # can we make it more flexible somehow?
     if len(message) < 2048:
         add_public_key( message )
-        #print(Fore.LIGHTMAGENTA_EX + "[+] received a public key" + Fore.RESET, "(", len(message), ") bytes long")
+        print(Fore.LIGHTMAGENTA_EX + "[+] received a public key" + Fore.RESET, "(", len(message), ") bytes long")
         
     else:
         add_message( message )
-        #print( Fore.LIGHTBLUE_EX + "[*] Received a packet:", len(message), "bytes long", Fore.RESET )
+        print( Fore.LIGHTBLUE_EX + "[*] Received a packet:", len(message), "bytes long", Fore.RESET )
